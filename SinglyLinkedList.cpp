8a9ef2801cb7922eb8a449c411419ed16ae1de90
dpaco <dpaco@hawaii.edu>
2022-04-25 15:24:00 -1000

    Defining SinglyLinkedList.cpp functions...

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -9,28 +9,79 @@
 /// @date   24_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include "SinglyLinkedList.h"
+using namespace std;
 
 SinglyLinkedList::SinglyLinkedList() {
 
 }
 
 void SinglyLinkedList::push_front(Node *newNode) {
+    if (newNode == nullptr){
+        throw invalid_argument("Error: newNode is nullptr");
+    }
+    if (!newNode -> validate()){
+        throw domain_error("Error: newNode is invalid");
+    }
+    if(isIn(newNode)){
+        throw logic_error("Error: newNode in container already");
+    }
 
+    if (head != nullptr){
+        newNode -> next = head;
+        head = newNode;
+    } else {
+        newNode -> next = nullptr;
+        head = newNode;
+    }
+
+    count++;
+    validate();
 }
 
 Node *SinglyLinkedList::pop_front() noexcept {
-    return nullptr;
+    if(head == nullptr){
+        return nullptr;
+    }
+    Node* value = head;
+    value -> next = nullptr;
+    count--;
+    validate();
+    return value;
 }
 
 void SinglyLinkedList::insert_after(Node *currentNode, Node *newNode) {
 
+    if(List::empty()){
+        throw logic_error("Error: List is empty");
+    }
+    if(currentNode == nullptr){
+        throw invalid_argument("Error: currentNode is nullptr");
+    }
+    if(isIn(newNode)){
+        throw logic_error("Error: newNode in container already");
+    }
+    if(!isIn(currentNode)){
+        throw logic_error("Error: currentNode not in list");
+    }
+    if(newNode == nullptr){
+        throw invalid_argument("Error: newNode is nullptr");
+    }
+
+    newNode -> next = currentNode -> next;
+    currentNode -> next = newNode;
+    count++;
+    validate();
 }
 
 void SinglyLinkedList::dump() const noexcept {
-
+    Node* printNode = head;
+    while (printNode != nullptr){
+        printNode -> dump();
+        printNode = printNode -> next;
+    }
 }
 
 bool SinglyLinkedList::validate() const noexcept {
-    return false;
+    if()
 }
 
